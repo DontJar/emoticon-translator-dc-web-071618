0b37e8283bb2c8fa0142ff78e22b6089bfb35113
Jon Schulman <schulman99@gmail.com>
2018-07-10 22:25:09 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(file)
 end
 
 def get_japanese_emoticon(file, emoji)
-    # translation_hash = load_library(file)
+    translation_hash = load_library(file)
     # translation = translation_hash["get_emoticon"][emoj]
     # translation
 end
